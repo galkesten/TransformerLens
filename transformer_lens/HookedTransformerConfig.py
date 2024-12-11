e323de092fe9e4e118b8f18c14fb4bd3973b1fd4
Gal Kesten <galkesten@newton.csf.technion.ac.il>
2024-12-11 23:29:11 +0200

    add attention

--- transformer_lens/HookedTransformerConfig.py
+++ transformer_lens/HookedTransformerConfig.py
@@ -262,6 +262,7 @@ class HookedTransformerConfig:
     NTK_by_parts_low_freq_factor: float = 1.0
     NTK_by_parts_high_freq_factor: float = 4.0
     NTK_by_parts_factor: float = 8.0
+    qk_layernorm: bool = False 
 
     def __post_init__(self):
         if self.n_heads == -1:
